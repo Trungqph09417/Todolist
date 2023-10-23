11b7ad5829799484d4ff9f41da67e04536947538
BaoTQ34 <baotq34@fpt.com>
2023-10-23 21:15:27 +0700

    todolist2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
 
 
 function App() {
+  const [work, setWork] = useState('')
+  console.log(work);
   return (
-    <div className="App">
-     app todolist
+    <div className="flex gap-2 h-screen border border-orange-400 justify-center items-center">
+     <input type="text" className="border outline-none border-blue-400 px-4 py-2"
+      value={work}
+      onChange={e => setWork(e.target.value)}
+       />
+     <button type="button" className="outline-none bg-blue-400 text-white px-4 py-2 rounded-md">Add</button>
     </div>
   );
 }
